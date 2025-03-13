LineageOS/android_packages_apps_Glimpse

Glimpse: Fix-up albums query
Android likes to translate everything to lower case...

Also properly group the bucket and get the higher
id within the bucket, as we assume it's the newest.

Change-Id: I3142e93302e5c6ae4d4f103ca697f948530798de

app/src/main/java/org/lineageos/glimpse/datasources/LocalDataSource.kt
@@ -49,12 +49,12 @@ class LocalDataSource(
         .build()
 
     private val mapAlbum = { columnIndexCache: ColumnIndexCache ->
-        val id = columnIndexCache.getLong(MediaStore.Files.FileColumns._ID)
+        val id = columnIndexCache.getLong(ALBUM_MAX_ID)
         val bucketId = columnIndexCache.getLong(MediaStore.Files.FileColumns.BUCKET_ID)
         val bucketDisplayName = columnIndexCache.getStringOrNull(
             MediaStore.Files.FileColumns.BUCKET_DISPLAY_NAME
         )
-        val count = columnIndexCache.getInt(ID_SQL_COUNT)
+        val count = columnIndexCache.getInt(ALBUM_COUNT_ID)
 
         val uri = ContentUris.withAppendedId(albumsUri, bucketId)
         val thumbnailUri = ContentUris.withAppendedId(filesUri, id)
@@ -210,6 +210,7 @@ class LocalDataSource(
             ).toTypedArray(),
             ContentResolver.QUERY_ARG_GROUP_COLUMNS to arrayOf(
                 MediaStore.Files.FileColumns.BUCKET_ID,
+                MediaStore.Files.FileColumns.BUCKET_DISPLAY_NAME,
             ),
             ContentResolver.QUERY_ARG_SORT_COLUMNS to arrayOf(
                 "${MediaStore.Files.FileColumns.DATE_MODIFIED} DESC",
@@ -267,17 +268,18 @@ class LocalDataSource(
     companion object {
         private const val ALBUMS_PATH = "albums"
 
-        private const val ID_SQL_COUNT = "COUNT(${MediaStore.Files.FileColumns._ID})"
-        private const val MAX_DATE_MODIFIED = "MAX(${MediaStore.Files.FileColumns.DATE_MODIFIED})"
+        private const val ALBUM_MAX_ID = "max(${MediaStore.Files.FileColumns._ID})"
+        private const val ALBUM_COUNT_ID = "count(${MediaStore.Files.FileColumns._ID})"
+        private const val ALBUM_MAX_DATE_MODIFIED = "max(${MediaStore.Files.FileColumns.DATE_MODIFIED})"
 
         private val albumProjection = arrayOf(
-            MediaStore.Files.FileColumns._ID,
+            ALBUM_MAX_ID,
             MediaStore.Files.FileColumns.BUCKET_ID,
             MediaStore.Files.FileColumns.BUCKET_DISPLAY_NAME,
-            ID_SQL_COUNT,
+            ALBUM_COUNT_ID,
 
             // GIANT HACK TO GET SQLITE TO ORDER BY DATE_MODIFIED
-            MAX_DATE_MODIFIED,
+            ALBUM_MAX_DATE_MODIFIED,
         )
 
         private val mediaProjection = arrayOf(

DroidX-UI/packages_apps_Glimpse

Signed-off-by: NRanjan-17 <nalinishranjan05@gmail.com>

app/src/main/java/org/lineageos/glimpse/datasources/LocalDataSource.kt
@@ -49,7 +49,7 @@ class LocalDataSource(
         .build()
 
     private val mapAlbum = { columnIndexCache: ColumnIndexCache ->
-        val id = columnIndexCache.getLong(MediaStore.Files.FileColumns._ID)
+        val id = columnIndexCache.getLong(ID_SQL_MAX)
         val bucketId = columnIndexCache.getLong(MediaStore.Files.FileColumns.BUCKET_ID)
         val bucketDisplayName = columnIndexCache.getStringOrNull(
             MediaStore.Files.FileColumns.BUCKET_DISPLAY_NAME
@@ -210,6 +210,7 @@ class LocalDataSource(
             ).toTypedArray(),
             ContentResolver.QUERY_ARG_GROUP_COLUMNS to arrayOf(
                 MediaStore.Files.FileColumns.BUCKET_ID,
+                MediaStore.Files.FileColumns.BUCKET_DISPLAY_NAME,
             ),
             ContentResolver.QUERY_ARG_SORT_COLUMNS to arrayOf(
                 "${MediaStore.Files.FileColumns.DATE_MODIFIED} DESC",
@@ -267,11 +268,12 @@ class LocalDataSource(
     companion object {
         private const val ALBUMS_PATH = "albums"
 
-        private const val ID_SQL_COUNT = "COUNT(${MediaStore.Files.FileColumns._ID})"
-        private const val MAX_DATE_MODIFIED = "MAX(${MediaStore.Files.FileColumns.DATE_MODIFIED})"
+        private const val ID_SQL_MAX = "max(${MediaStore.Files.FileColumns._ID})"
+        private const val ID_SQL_COUNT = "count(${MediaStore.Files.FileColumns._ID})"
+        private const val MAX_DATE_MODIFIED = "max(${MediaStore.Files.FileColumns.DATE_MODIFIED})"
 
         private val albumProjection = arrayOf(
-            MediaStore.Files.FileColumns._ID,
+            ID_SQL_MAX,
             MediaStore.Files.FileColumns.BUCKET_ID,
             MediaStore.Files.FileColumns.BUCKET_DISPLAY_NAME,
             ID_SQL_COUNT,